e92481f9dfa7807ad91837d47c1e545a2fcf4f6d
codeuiandy <frostandy41@gmail.com>
2022-02-15 15:40:45 +0100

    updates

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -34,7 +34,7 @@ export default function Navbar() {
 
           <li
             className={`${
-              location.pathname == "/hmenu" ? "activeNvaLink" : ""
+              location.pathname == "/about-us" ? "activeNvaLink" : ""
             }`}
           >
             <Link to="/about-us">About us</Link>
@@ -42,7 +42,7 @@ export default function Navbar() {
 
           <li
             className={`${
-              location.pathname == "/hmenu" ? "activeNvaLink" : ""
+              location.pathname == "/contact" ? "activeNvaLink" : ""
             }`}
           >
             <Link to="/contact">Contact </Link>
@@ -54,22 +54,82 @@ export default function Navbar() {
         <div className="mobileLinks">
           <ul>
             <li
+              className={`${location.pathname == "/" ? "activeNvaLink" : ""}`}
+            >
+              <Link
+                onClick={() => {
+                  setShowNav(false);
+                }}
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li
               className={`${
-                location.pathname == "/hmenu" ? "activeNvaLink" : ""
+                location.pathname == "/about-us" ? "activeNvaLink" : ""
               }`}
             >
-              <Link to="/">Home</Link>
+              <Link
+                onClick={() => {
+                  setShowNav(false);
+                }}
+                to="/about-us"
+              >
+                About us
+              </Link>
             </li>
             <li
-              className={`${location.pathname == "/h" ? "activeNvaLink" : ""}`}
+              className={`${
+                location.pathname == "/contact" ? "activeNvaLink" : ""
+              }`}
             >
-              <Link to="/">About us</Link>
-            </li>
-            <li
-              className={`${location.pathname == "/h" ? "activeNvaLink" : ""}`}
-            >
-              <Link to="/">Contact</Link>
+              <Link
+                onClick={() => {
+                  setShowNav(false);
+                }}
+                to="/contact"
+              >
+                Contact
+              </Link>
             </li>{" "}
+            <li>
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20.951"
+                  height="23.241"
+                  viewBox="0 0 20.951 23.241"
+                >
+                  <path
+                    id="Icon_awesome-google-play"
+                    data-name="Icon awesome-google-play"
+                    d="M15.4,10.638,5.379.59,18.128,7.909,15.4,10.638ZM2.764,0a1.765,1.765,0,0,0-.985,1.6V21.638a1.765,1.765,0,0,0,.985,1.6l11.65-11.623Zm19.3,10.243L19.395,8.694l-2.983,2.928,2.983,2.928L22.123,13a1.821,1.821,0,0,0-.054-2.76ZM5.379,22.655l12.749-7.319L15.4,12.608Z"
+                    transform="translate(-1.779)"
+                    fill="#fcfcfc"
+                  />
+                </svg>
+                <span> Get on Play Store</span>
+              </button>
+              <button>
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20.951"
+                  height="23.241"
+                  viewBox="0 0 20.951 23.241"
+                >
+                  <path
+                    id="Icon_awesome-google-play"
+                    data-name="Icon awesome-google-play"
+                    d="M15.4,10.638,5.379.59,18.128,7.909,15.4,10.638ZM2.764,0a1.765,1.765,0,0,0-.985,1.6V21.638a1.765,1.765,0,0,0,.985,1.6l11.65-11.623Zm19.3,10.243L19.395,8.694l-2.983,2.928,2.983,2.928L22.123,13a1.821,1.821,0,0,0-.054-2.76ZM5.379,22.655l12.749-7.319L15.4,12.608Z"
+                    transform="translate(-1.779)"
+                    fill="#fcfcfc"
+                  />
+                </svg>{" "}
+                <span>Get on App Store</span>
+              </button>
+            </li>
           </ul>
         </div>
       ) : (
